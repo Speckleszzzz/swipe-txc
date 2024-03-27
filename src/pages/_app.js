08af0bac0d46e61ce518f8604f921766bb969347
Speckles zzzz <speckleszzzz@gmail.com>
2024-03-27 09:41:40 +0000

    rainbowKit Setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,31 @@
 import "@/styles/globals.css";
+import '@rainbow-me/rainbowkit/styles.css';
+import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [chain.mainnet],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'RainbowKit Demo',
+});
+
+const WagmiClient = createClient({
+  connectors,
+  provider
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(
+  <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider>
+        <NavBar></NavBar>
+      </RainbowKitProvider>
+    </QueryClientProvider>
+  </WagmiProvider>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,20 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import NavBar from "@/components/NavBar";
+import '@rainbow-me/rainbowkit/styles.css';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-      <NavBar></NavBar>
+      <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <NavBar></NavBar>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
     </main>
   );
 }
