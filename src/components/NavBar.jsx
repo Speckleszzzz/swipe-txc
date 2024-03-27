d627eb95330029083318535c22ace562e2644172
Speckles zzzz <speckleszzzz@gmail.com>
2024-03-27 09:22:44 +0000

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default function NavBar() {
+    return(
+        <div>
+            <div className='flex justify-between px-5 h-vh[10] items-center bg-white opacity-80 mx-20 rounded-full'>
+                <div>
+                    <p className=' text-black'>Logo</p>
+                </div>
+                <div className=' text-black flex justify-center space-x-4'>
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Contact</p>
+                </div>
+                <div className='text-black rounded-full'>
+                    <button>Connect Wallet</button>
+                </div>
+            </div>
+        </div>
+    )
+}
